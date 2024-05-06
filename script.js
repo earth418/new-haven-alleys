bc6cb6ef405063552471d2c77bea30b9f4d192fe
Ali Hafez <alihafez42@gmail.com>
2024-05-06 01:09:32 -0400

    bubble fix

--- script.js
+++ script.js
@@ -139,25 +139,26 @@
 				// d3.selectAll(".g-onLoad_A").classed('hidden', false);
                 // d3.select(".sticky-thing").style("height", "0px");
 			}
-		})
+		});
 
         d3.select("#step-four").on('stepin', function(e) {
+            if (e.detail.direction == 'up') {
+                
+            }
             d3.selectAll("#s5_A").classed('invisible', true);
+            d3.select(".ai2html-container").style("transform", "scale(1.0) translate(0.0)");
             d3.select(".ai2html").style("transform", "scale(1.0)");
-        });
-
-        d3.select("#step-four").on('stepin', function(e) {
-            d3.selectAll("#s5_A").classed('invisible', true);
-            d3.select(".ai2html").style("transform", "scale(1.0)");
+            console.log("Hey!!");
         });
 
 
         d3.select("#step-five").on('stepin',function(e) {
             d3.selectAll("#s5_A").classed('invisible', false);
             d3.select(".ai2html-container").classed("invisible", false);
-            d3.select(".ai2html").style("transform", "scale(3.0) translate(-25%,-25%)");
+            d3.select(".ai2html").style("transform", "scale(5.0) translate(-30%,-25%)");
             
-            d3.select(".ai2html-container").style("transform", "scale(0.5) translate(50%,47%)");
+            d3.select(".ai2html-container").style("transform", "scale(0.40)");
+            d3.select(".ai2html-container").style("margin-bottom", "1%");
             // d3.selectAll("#s1_A").classed('hidden', true); // mory's stuff
         });
         
